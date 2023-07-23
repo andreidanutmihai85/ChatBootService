c4670d621006ce73c0cdb54529ad35da1e2c71d0
Andrei Danut Mihai <138859815+andreidanutmihai85@users.noreply.github.com>
2023-07-23 10:27:34 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,133 @@
+
+
+
+
+
+
+// let buton1 = document.getElementById("buton1");
+// let buton2 = document.getElementById("buton2");
+
+let intrebare1 = document.getElementById("intrebare1");
+let intrebare2 = document.getElementById("intrebare2");
+let intrebare3 = document.getElementById("intrebare3");
+
+let messageRight = document.getElementById("mesaj1");
+let messageRight2 = document.getElementById("mesaj2");
+let messageRight3 = document.getElementById("mesaj3");
+let dots = document.getElementById("dots");
+let dots2 = document.getElementById("dots2")
+let dots3 = document.getElementById("dots3")
+let chat_id = document.getElementById("chat_id");
+
+let divButtons = document.getElementsByClassName("chat-container");
+
+
+
+
+const elements = [];
+
+
+
+
+
+
+function function1(){
+
+    if(elements == 0){
+        elements.push(intrebare1.style.display = "block") ;
+        return elements;
+    }else{
+
+        elements.push(intrebare1.style.display = "block") ;
+        
+        return elements;
+    }
+}
+
+
+function function1_0(){
+    const ras1 = messageRight.style.display = "block";
+    const ras2 = dots.style.display = "none";
+    return ras1, ras2;
+};
+       
+
+
+
+    function myFunction() {
+    
+            let elements = intrebare2.style.display = "block" ;
+            let elementsClear = elements.slice();
+            return intrebare1.remove(),messageRight.remove(), elementsClear, elements;
+
+        }
+
+
+
+        
+
+function function2(){
+    if(elements == 0){
+        elements.push(intrebare2.style.display = "block") ;
+        return elements
+    }else{
+      myFunction();
+    
+    
+    }
+}
+
+function function2_0(){
+    const ras1 = messageRight2.style.display = "block";
+    const ras2 = dots2.style.display = "none";
+    return ras1, ras2;
+ 
+};
+
+
+
+function myFunction2() {
+
+    let elements = intrebare3.style.display = "block" ;
+    let elementsClear = elements.split(0);
+
+    return intrebare1.remove(), messageRight.remove(),intrebare2.remove(), messageRight2.remove(),elementsClear, elements;
+
+}
+
+
+
+
+function function3(){
+    if(elements == 0){
+        elements.push(intrebare3.style.display = "block") ;
+        return elements
+    }else{
+      myFunction2();
+
+    
+      
+
+    
+    }
+
+}
+
+function function3_0(){
+ 
+    const ras1 = messageRight3.style.display = "block";
+    const ras2 = dots3.style.display = "none";
+    return ras1, ras2;
+ 
+};
+
+
+
+
+
+
+
+
+
+
+
